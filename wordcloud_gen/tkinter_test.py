84b7a891c22eda2e59549ec4389636c3404f3a6b
Ricardo <ric@no_server.org>
2015-05-22 14:17:56 +0100

    Implement some basic image transitions via periodic blend

--- wordcloud_gen/tkinter_test.py
+++ wordcloud_gen/tkinter_test.py
@@ -19,31 +19,64 @@ def main():
     screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
     master = Tk()
-    master.state('zoomed')
+    master.state('zoomed') # maximize the window
 
     canvas = Canvas(master, width=screensize[0], height=screensize[1])
     canvas.pack()
-            
-    master.after(1000, task)
+
+    img = generateImage()
+    putImageOnCanvas(img)
+    
+    master.after(1000, doUpdate, img)
     master.mainloop()
 
-def task():
-    global image_ref # used to keep python's GC from screwing with our images
-    
-    text = open(sys.argv[1]).read()
+old_wc = None
+
+def generateImage():
+    with open(sys.argv[1]) as f: 
+        text = f.read()
+
     wc = WordCloud(font_path='C:\Windows\Fonts\Verdana.ttf', width=750, height=400).generate(text)
     wc = wc.recolor(color_func=grey_color_func, random_state=3)
-    wc_img = wc.to_image()
+    img = wc.to_image()
     
     scale = 0.5 # % of the whole screen
     size = tuple(int(i * scale) for i in screensize)
-    wc_img = wc_img.resize(size, Image.ANTIALIAS)
+    img = img.resize(size, Image.ANTIALIAS)
+    return img
 
-    image_ref = ImageTk.PhotoImage(wc_img)
+def putImageOnCanvas(img):
+    global image_ref # used to keep python's GC from screwing with our images
+    image_ref = ImageTk.PhotoImage(img)
     canvas.create_image((screensize[0]/2, screensize[1]/2), image=image_ref, state="normal")
+    
+def DoShit():
+    print "Actual DoShit call"
+    global old_wc
+    wc_img = generateImage()
+    (x, y) = old_wc
+    old_wc = (x, wc_img)
+    
+    
+    putImageOnCanvas(old_wc[0])
+    master.after(4000, DoShit2)
+    
+alpha = 0.0
 
-    master.after(5000, task)
+def doUpdate(oldImg):
+    newImg = generateImage()
+    
+    master.after(1, doImageTransition, (oldImg, newImg), 0.0)
 
+def doImageTransition((oldImg, newImg), alpha):
+    img = Image.blend(oldImg, newImg, alpha)
+    putImageOnCanvas(img)
+    
+    if alpha >= 0.99:
+        master.after(5000, doUpdate, newImg)
+    else:
+        master.after(50, doImageTransition, (oldImg, newImg), alpha + 0.05)
+    
 def grey_color_func(word, font_size, position, orientation, random_state=None):
     return "hsl(0, 0%%, %d%%)" % 75
 
