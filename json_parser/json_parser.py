824705d032d1b22f2fb8e424e948f77ad4d43c7e
Ricardo <ric@no_server.org>
2015-05-24 16:15:20 +0100

    Adapt json parser/text feed/text preprocess scripts for a pipe-based execution

--- json_parser/json_parser.py
+++ json_parser/json_parser.py
@@ -2,24 +2,22 @@
 import json
 import sys
 
-(transcript, confidence) = ("", 0.0)
-
-with open(sys.argv[1], 'r') as jsonfile:
-    lines = jsonfile.readlines()
-
-    values = { }
-    # response can be empty
-    if len(lines) > 1:
-        # lines[1] since the first line is an empty result
-        # the 2 lines combined do not form a valid json structure, but each line itself is a json structure
-        values = json.loads(lines[1])
+# each input line should be a valid json object
+# just parse them one by one, discard json objects without the fields we need, and print out the useful info
+while True:
+    (transcript, confidence) = ("", 0.0)
+    jsonline = sys.stdin.readline()
+    values = json.loads(jsonline)
 
     try:
         transcript = values['result'][0]['alternative'][0]['transcript']
         confidence = values['result'][0]['alternative'][0]['confidence']
     except KeyError:
         pass
+    except IndexError:
+        pass
 
-# we don't actually use the confidence for anything, but it's an interesting value
-# print confidence
-print transcript
+    # we don't actually use the confidence for anything, but it's an interesting value
+    # print confidence
+    print transcript
+    sys.stdout.flush() # force flush so the next program immediately gets the print, in case we're in a pipe
--- testing/text_feed_script.py
+++ testing/text_feed_script.py
@@ -1,21 +1,15 @@
 
 import sys
+import time
 
 # file location should be in the first arg
 filename = sys.argv[1]
+period = int(sys.argv[2])
 
-f = open(filename, "r")
-lines = f.readlines()
-f.close
+with open(filename, "r") as f:
+    for line in f:
+        print line.rstrip()
+        sys.stdout.flush() # force flush so the next program immediately gets the print, in case we're in a pipe
+        # sleep for the period time
+        time.sleep(period)
 
-lastline = lines.pop() # remove last line
-
-# reopen the file in write mode
-f = open(filename, "w")
-
-for line in lines:
-    f.write(line)
-    
-f.close()
-
-print lastline.rstrip()
